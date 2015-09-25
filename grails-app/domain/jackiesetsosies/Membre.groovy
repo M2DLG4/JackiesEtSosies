d70e41dea6a3d1a1aefab5c182fd5252ee47d8f3
Lucas B <lucas.bled12@gmail.com>
2015-09-25 14:52:50 +0200

    #12 #13 #14 Some modification

--- grails-app/domain/jackiesetsosies/Membre.groovy
+++ grails-app/domain/jackiesetsosies/Membre.groovy
@@ -1,7 +1,26 @@
 package jackiesetsosies
-
+/*
+    Modele du Membre
+ */
 class Membre {
+    String nom;
+    String prenom;
+    String mail;
+    String mdp;
+    String ville;
+    String sexe;
+    boolean isSosie;
+    Star idStar;
+    Photo idPhoto;
 
     static constraints = {
+        nom blank: false
+        prenom blank: false
+        mail email: true
+        ville blank: false
+        sexe inList: ["F", "M"]
+        isSosie nullable: false
+        idStar nullable: true
+        idPhoto nullable: true
     }
 }
--- grails-app/domain/jackiesetsosies/Star.groovy
+++ grails-app/domain/jackiesetsosies/Star.groovy
@@ -1,7 +1,15 @@
 package jackiesetsosies
-
+/*
+    Modele du Star
+ */
 class Star {
+    String nom;
+    String prenom;
+    Photo urlPhoto;
 
     static constraints = {
+        nom blank: false
+        prenom blank: false
+        urlPhoto blank: false
     }
 }
