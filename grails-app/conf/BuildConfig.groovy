f05e97f57acc1649be8012c5c43a5bb43353780e
Tetote <theo.vaucher@gmail.com>
2015-10-01 09:24:25 +0200

    Add coverage

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -80,5 +80,7 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.5.3"
         //compile ":coffee-asset-pipeline:1.5.0"
         //compile ":handlebars-asset-pipeline:1.3.0.1"
+
+        test ":code-coverage:2.0.3-3"
     }
 }
