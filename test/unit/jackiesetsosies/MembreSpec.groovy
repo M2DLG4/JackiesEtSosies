ce8a4866d1ac5abb547dcd3d86a4f8882d1201c3
Lucas B <lucas.bled12@gmail.com>
2015-10-02 16:40:54 +0200

    #28 Code coverage

--- test/unit/jackiesetsosies/MembreSpec.groovy
+++ test/unit/jackiesetsosies/MembreSpec.groovy
@@ -73,4 +73,86 @@ class MembreSpec extends Specification {
         "Jacky" | "Pierre" | "pierre.jacky@gmail.com" | "JohnnyLeBest" | "H"  | ""
         "Jacky" | "Pierre" | "pierre.jacky@gmail.com" | "JohnnyLeBest" | "H"  | null
     }
+
+    @Unroll
+    void "test toString"() {
+        given:"un Mock star et un membre avec un nom, prenom, mail, mdp, sexe, ville, isSosie, urlPhoto"
+        def star = Mock(Star)
+        def membre = new Membre(nom: "Halliday", prenom: "Johnny", mail: "john.hal@hal.fr", mdp: "aze",
+                                sexe: "M", ville: "Paris", isSosie: false, urlPhoto: "john.jpg", idStar: star);
+
+        when:"on affiche le membre"
+        def toString = membre.toString()
+
+        then:"le resultat affiché est le bon"
+        toString=="Membre{" +
+                "nom='" + "Halliday" + '\'' +
+                ", prenom='" + "Johnny" + '\'' +
+                ", mail='" + "john.hal@hal.fr" + '\'' +
+                ", mdp='" + "aze" + '\'' +
+                ", ville='" + "Paris" + '\'' +
+                ", sexe='" + "M" + '\'' +
+                ", isSosie=" + "false" +
+                ", idStar=" + star.toString() +
+                ", urlPhoto='" + "john.jpg" + '\'' +
+                '}';
+    }
+
+    @Unroll
+    void "test equals"() {
+        given:"Deux membres avec la même adresse mail"
+        def membre1 = new Membre(nom: _, prenom: _, mail: "john.hal@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+        def membre2 = new Membre(nom: _, prenom: _, mail: "john.hal@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+        def membre3 = new Membre(nom: _, prenom: _, mail: "john@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+
+        when:"on cherche à savoir si membre1 et membre2 sont les mêmes"
+        def isEquals = membre1.equals(membre2)
+
+        then:"ils sont les mêmes"
+        isEquals
+
+        when:"on cherche à savoir si membre1 est égal à lui-même"
+        isEquals = membre1.equals(membre1)
+
+        then:"il est égal à lui-même"
+        isEquals
+
+        when:"on cherche à savoir si membre1 et membre3 sont les mêmes"
+        isEquals = membre1.equals(membre3)
+
+        then:"ils ne sont pas les mêmes"
+        !isEquals
+
+        when:"on cherche à savoir si membre1 et une instance d'autre classe sont les mêmes"
+        isEquals = membre1.equals(Mock(Star))
+
+        then:"ils ne sont pas les mêmes"
+        !isEquals
+    }
+
+    @Unroll
+    void "test hashCode"() {
+        given:"Deux membres avec la même adresse mail et un autre avec une adresse mail différente"
+        def membre1 = new Membre(nom: _, prenom: _, mail: "john.hal@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+        def membre2 = new Membre(nom: _, prenom: _, mail: "john.hal@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+        def membre3 = new Membre(nom: _, prenom: _, mail: "john@hal.fr", mdp: _,
+                sexe: _, ville: _, isSosie: _,);
+
+        when:"on cherche à savoir si membre1 et membre2 ont le même HashCode"
+        def sameHashCode = (membre1.hashCode() == membre2.hashCode())
+
+        then:"ils sont les mêmes"
+        sameHashCode
+
+        when:"on cherche à savoir si membre1 et membre3 ont le même HashCode"
+        sameHashCode = (membre1.hashCode() == membre3.hashCode())
+
+        then:"ils ne sont pas les mêmes"
+        !sameHashCode
+    }
 }
