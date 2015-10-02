c05ff3d86db8008681e05fb2490bdde014989b50
Tetote <theo.vaucher@gmail.com>
2015-10-02 16:39:01 +0200

    Add test for Star #13

--- test/unit/jackiesetsosies/StarSpec.groovy
+++ test/unit/jackiesetsosies/StarSpec.groovy
@@ -42,6 +42,122 @@ class StarSpec extends Specification {
         "Hallyday" | null     | "machin.jpg"
         "Hallyday" | "Johnny" | ""
         "Hallyday" | "Johnny" | null
+    }
 
+    @Unroll
+    void "test toString"() {
+        given: "une star avec un prenom, nom, url en parametre"
+        def nom = "Hallyday"
+        def prenom = "Johnny"
+        def urlPhoto = "machin.jpg"
+        def star = new Star(prenom: prenom, nom: nom, urlPhoto: urlPhoto)
+
+        when: "on affiche le toString"
+        def toString = star.toString()
+
+        then: "le résultat est bien celui attendu"
+        toString == "Star{" +
+                    "nom='" + nom + '\'' +
+                    ", prenom='" + prenom + '\'' +
+                    ", urlPhoto='" + urlPhoto + '\'' +
+                    '}'
+    }
+
+    @Unroll
+    void "test equals"() {
+        given: "une star avec un prenom, nom, url en parametre"
+        def nom1 = "Hallyday"
+        def prenom1 = "Johnny"
+        def urlPhoto1 = "machin1.jpg"
+        def star1 = new Star(prenom: prenom1, nom: nom1, urlPhoto: urlPhoto1)
+
+        and: "une autre star avec un prenom, nom, url en parametre"
+        def nom2 = "Sebastien"
+        def prenom2 = "Patrick"
+        def urlPhoto2 = "machin2.jpg"
+        def star2 = new Star(prenom: prenom2, nom: nom2, urlPhoto: urlPhoto2)
+
+        when: "on compare deux stars différentes"
+        def isEquals = star1.equals(star2)
+
+        then: "le résultat est faux"
+        !isEquals
+
+        when: "on compare les mêmes stars"
+        isEquals = star1.equals(star1)
+
+        then: "le résultat est vrai"
+        isEquals
+
+        when: "on compare la star avec une autre classe"
+        isEquals = star1.equals(new Integer(42))
+
+        then: "le résultat est faux"
+        !isEquals
+
+        when: "on compare la star avec un nom différent"
+        star2.prenom = "Johnny"
+        star2.nom = "aaaa"
+        star2.urlPhoto = "machin1.jpg"
+        isEquals = star1.equals(star2)
+
+        then: "le résultat est faux"
+        !isEquals
+
+        when: "on compare la star avec un prenom différent"
+        star2.prenom = "aaaaa"
+        star2.nom = "Hallyday"
+        star2.urlPhoto = "machin1.jpg"
+        isEquals = star1.equals(star2)
+
+        then: "le résultat est faux"
+        !isEquals
+
+        when: "on compare la star avec une photo différent"
+        star2.prenom = "Johnny"
+        star2.nom = "Hallyday"
+        star2.urlPhoto = "machin2.jpg"
+        isEquals = star1.equals(star2)
+
+        then: "le résultat est faux"
+        !isEquals
+
+        when: "on compare la star avec les mêmes infos"
+        star2.prenom = "Johnny"
+        star2.nom = "Hallyday"
+        star2.urlPhoto = "machin1.jpg"
+        isEquals = star1.equals(star2)
+
+        then: "le résultat est vrai"
+        isEquals
+    }
+
+    @Unroll
+    void "test hashCode"() {
+        given: "une star avec un prenom, nom, url en parametre"
+        def nom1 = "Hallyday"
+        def prenom1 = "Johnny"
+        def urlPhoto1 = "machin1.jpg"
+        def star1 = new Star(prenom: prenom1, nom: nom1, urlPhoto: urlPhoto1)
+
+        and: "une autre star avec un prenom, nom, url en parametre"
+        def nom2 = "Sebastien"
+        def prenom2 = "Patrick"
+        def urlPhoto2 = "machin2.jpg"
+        def star2 = new Star(prenom: prenom2, nom: nom2, urlPhoto: urlPhoto2)
+
+        when: "on calcule le hashCode de deux stars différentes"
+        def hashCodeStar1 = star1.hashCode()
+        def hashCodeStar2 = star2.hashCode()
+
+        then: "le hashCode est différent"
+        hashCodeStar1 != hashCodeStar2
+
+        when: "on calcule le hashCode de la même star"
+        def hashCodeStar11 = star1.hashCode()
+        def hashCodeStar12 = star1.hashCode()
+
+        then: "le hashCode est le même"
+        hashCodeStar11 == hashCodeStar12
     }
 }
