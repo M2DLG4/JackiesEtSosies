999da7845a7f206135c885f417c87ec7e68e8660
superkebab <felgines.elliot@gmail.com>
2015-10-28 09:11:17 +0100

    Ajout élément bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -13,7 +13,7 @@ class BootStrap {
         Star star5 = new Star(prenom: "Barack", nom:"Obama", urlPhoto:"http://mosaiqueguinee.com/wp-content/uploads/2015/03/obama.jpg").save()
         Star star6 = new Star(prenom: "Barbie", nom:"Pifias", urlPhoto:"http://1.bp.blogspot.com/-Cij0ZaLCGcw/UPHZ9OxFEgI/AAAAAAAAAKE/PJmo7ruqhcg/s1600/Barbie+Face.jpg").save()
         Star star7 = new Star(prenom: "Paul", nom:"Van Haver", urlPhoto:"http://www.forumopera.com/sites/default/files/stromae_0.jpg").save()
-
+        Star star8 = new Star(prenom: "Nicolas", nom:"Sarkozy", urlPhoto:"http://www.gaulhore.fr/wp-content/uploads/2015/09/Nicolas_Sarkozy.jpg").save()
 
         Membre membre1 = new Membre(prenom: "Patrice", nom:"Perdu", mail: "pat.perdu@wanadoo.net", mdp: "1234", ville:"Chalon-Sur-Saulnes", sexe:"H", urlPhoto:"http://www.cesam-international.com/_upload/ressources/photos/patrick_bruel__jerome/jeromebruel.jpg", isSosie: true, idStar: star3.id).save()
         Membre membre2 = new Membre(prenom: "Carl", nom:"Raté", mail: "carlo.el.bandito@laposte.net", mdp: "1234", ville:"Champagne-Sur-Ardennes", sexe:"H", urlPhoto:"http://img1.gtsstatic.com/insolite/le-sosie-de-gerard-depardieu-fredo-de-belgique-en-photo-dans-une-soiree_114289_w620.jpg", isSosie: true, idStar: star4.id).save()
@@ -21,6 +21,7 @@ class BootStrap {
         Membre membre4 = new Membre(prenom: "Jean", nom:"Albert", mail: "jean.albert@gmail.com", mdp: "1234", ville:"Berlin Est", sexe:"F", urlPhoto:"http://photos1.tf1.fr/260/346/melanie-secret-story-9-b53b89-1@1x.jpg", isSosie: false).save()
         Membre membre5 = new Membre(prenom: "Patrick", nom:"Jeannon", mail: "patrick.jeannon@monster.fr", mdp: "1234", ville:"Ordure sur mer", sexe:"H", urlPhoto:"http://photos1.tf1.fr/260/346/karisma-secret-story-9-2-80e787-1@1x.jpg", isSosie: false).save()
         Membre membre6 = new Membre(prenom: "Dark", nom:"Vador", mail: "dark@vador.fr", mdp: "1234", ville:"Planette Noire", sexe:"H", urlPhoto:"http://a403.idata.over-blog.com/300x295/0/01/23/60/2012/dark-vador-essuie-ton-pere-star-wars-rouleau-essuie-ton-per.jpg", isSosie: true, idStar: star2.id).save()
+        Membre membre7 = new Membre(prenom: "Philippe", nom:"Cobra", mail: "philippe@vazy.fr", mdp: "1234", ville:"Hong-Kong", sexe:"H", urlPhoto:"http://www.masculin.com/images/article/9289/sosie-de-merde-sarko-2.jpg", isSosie: true, idStar: star8.id).save()
 
         Post post1 = new Post(date: new Date(), message: "Qui a le droit ?", nbLikes: 0, membre: membre1).save()
         Post post2 = new Post(date: new Date(), message: "Moi aussi un jour je serais vigneron comme le Grand Gérard ! J'espère avoir mon passeport russe aussi.", nbLikes: 0, membre: membre2).save()
